07dace4b935605c31580d5028d2062a1ad3840b3
Pritam Roy <pritamroy1136@gmail.com>
2023-11-28 23:44:17 +0530

    Login process done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -23,4 +23,21 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(404).json("Invalid password");
+    res.send(200).json("User found");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;

ebd22598d44de57e1dd2f79462e5afdfe8a28528
Pritam Roy <pritamroy1136@gmail.com>
2023-11-30 19:09:30 +0530

    Completed feed section

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,6 +1,45 @@
 import React from "react";
 import "./share.css";
+import PermMediaIcon from "@mui/icons-material/PermMedia";
+import LabelIcon from "@mui/icons-material/Label";
+import LightIcon from "@mui/icons-material/Light";
+import TagFacesIcon from "@mui/icons-material/TagFaces";
 
 export default function Share() {
-  return <div>Share</div>;
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img src="/assets/person/1.jpeg" alt="" className="shareProfileImg" />
+          <input
+            type="text"
+            className="shareInput"
+            placeholder="What's in your mind?"
+          />
+        </div>
+        <hr className="shareHr" />
+        <div className="shareBottom">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <PermMediaIcon htmlColor="tomato" className="shareIcon" />
+              <span className="shareOptionText">Photo</span>
+            </div>
+            <div className="shareOption">
+              <LabelIcon htmlColor="blue" className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <LightIcon htmlColor="green" className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <TagFacesIcon htmlColor="goldenrod" className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
+            </div>
+          </div>
+          <div className="shareButton">Share</div>
+        </div>
+      </div>
+    </div>
+  );
 }

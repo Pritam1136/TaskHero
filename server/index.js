579f342e06a790e537b659e77d4a97df5885fa5a
Pritam Roy <pritamroy1136@gmail.com>
2023-12-04 19:05:46 +0530

    Added multer for file upload

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const userRoute = require("./routes/users");
 const authroute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const app = express();
+const multer = require("multer");
 
 dotenv.config();
 
@@ -18,11 +19,29 @@ try {
   console.error(err);
 }
 
-// middleware
+// middlewares
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("file uploaded successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authroute);
